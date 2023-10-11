adbc6efc6d56782fafd959b3ae56adaf13501b77
Luizaescarban <luizaescarbandasil@gmail.com>
2023-10-11 16:37:50 -0300

    responsividade quase pronta

--- script.js
+++ script.js
@@ -66,3 +66,14 @@ function limparFormulario() {
     document.getElementById("imc").innerHTML = "";
     document.getElementById("comentario").innerHTML = "";
 }
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
